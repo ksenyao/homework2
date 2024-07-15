038b0ec76bd4fc9e72db436d8dde1b0654c66bf1
Ксения Че <chechulina75@list.ru>
2024-07-15 21:17:05 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+amount_of_homework=12
+number_of_hours=1.5
+name_of_the_course= 'Python'
+time_one_task=(number_of_hours/amount_of_homework)
+print('Курс:',name_of_the_course ,',всего задач:',amount_of_homework, ', затрачено часов:',number_of_hours, ', cреднее время выполнения',time_one_task, 'часа.')
